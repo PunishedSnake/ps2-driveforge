320c008af499f996b4d40dcd854f2cb10fe035c1
Hifu <artix1995@gmail.com>
2026-08-22 00:12:38 +0200

    Include generated WinUI window factory

--- src/winui/MainWindow.xaml.cpp
+++ src/winui/MainWindow.xaml.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 #include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
 
 namespace winrt::PS2DriveForge::WinUI::implementation
 {
