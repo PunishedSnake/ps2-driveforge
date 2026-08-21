871fd140f37a63002df44e8689dbb0fb70676f91
Hifu <artix1995@gmail.com>
2026-08-21 21:54:06 +0200

    Emilia: hide native offset file backend behind FileBlockDevice pimpl

--- include/ps2hdd/file_block_device.hpp
+++ include/ps2hdd/file_block_device.hpp
@@ -3,14 +3,17 @@
 #include "ps2hdd/block_device.hpp"
 
 #include <filesystem>
-#include <fstream>
-#include <mutex>
+#include <memory>
 
 namespace ps2hdd {
 
 class FileBlockDevice final : public BlockDevice {
 public:
     explicit FileBlockDevice(std::filesystem::path path);
+    ~FileBlockDevice() override;
+
+    FileBlockDevice(const FileBlockDevice&) = delete;
+    FileBlockDevice& operator=(const FileBlockDevice&) = delete;
 
     [[nodiscard]] bool is_open() const noexcept;
     [[nodiscard]] std::uint64_t size_bytes() const override;
@@ -18,10 +21,11 @@ public:
     bool read(std::uint64_t offset, std::span<std::byte> out) override;
 
 private:
+    struct Impl;
+
     std::filesystem::path path_;
-    std::ifstream stream_;
+    std::unique_ptr<Impl> impl_;
     std::uint64_t size_{};
-    mutable std::mutex mutex_;
 };
 
 } // namespace ps2hdd
